757566c619c868811998d7dcbe49cb2311ae3c6f
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-01-25 14:16:25 -0500

    creaded this.state, wrote Game methods

--- src/components/game.js
+++ src/components/game.js
@@ -7,7 +7,50 @@ import GuessList from './guess-list';
 //change feedback to pass hot or cold (from state)
 //change count from state and pass to GuessCount
 //grab guesses from state and pass to GuessList
-export default function Game(props) {
+export default class Game extends React.Component {
+   constructor(props){
+    super(props)
+
+    this.state={
+        guessArray:[],
+        isHot: null,
+        correctAnswer: null,
+        whatPage: false
+    }
+
+   }
+   handleAddGuess(guess){
+       this.toggleHotCold(guess);
+       this.setState({
+        guessArray:[...this.state.guessArray, guess]
+       })
+   }
+
+   createCorrectAnswer(){
+    const newGameAnswer = Math.floor(Math.Random()*100)+1
+       this.setState({correctAnswer: newGameAnswer})
+   }
+
+   toggleHotCold(guess){
+      const correctAnswer = this.state.correctAnswer;
+      if(guess < correctAnswer + 10 && guess > correctAnswer -10){
+          this.setState({
+              isHot: true
+          })
+      } else { this.setState({
+          isHot: false
+        })
+      }
+   }
+
+   toggleWhatPage(){
+        let notWhatPage = !(this.state.whatPage)
+         this.setState({whatPage: notWhatPage}) 
+       
+   }
+   
+   
+    render(){
     return (
         <div>
             <Header />
@@ -16,5 +59,6 @@ export default function Game(props) {
             <GuessList guesses={[10, 15, 25]} />
         </div>
     );
+   }
 }
 
